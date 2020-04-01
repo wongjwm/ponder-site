82d528d337bd3d8ba1b6aab126e6711393b3a456
Judy W <judy.wm.wong@gmail.com>
2020-04-01 11:21:42 -0400

    Add content

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const AboutContainer = () => (
-  <div className='home'>
-    <h1>About Ponder</h1>
-    <p> Feel free to browse around and learn more about me.</p>
+  <div className='about'>
+    <h2>About Ponder</h2>
+    <p>Ponder is a year-long senior capstone project by 3 undergraduate students at Northeastern University. The project focuses on high schoolers and data privacy. Through surveys and interviews conducted on high schoolers in Boston, different perspectives were gathered and the gaps in data privacy education were identified. While the average high schooler is quite aware of their online presence in some aspects, in others, the education is lacking.</p>
+    <p>This project is crucial due to the place the younger generation holds in the technological space and in society. They are tech-natives who are fully and uniquely immersed in the modern online ecosystem. They are poised to inherit the complex policies and ethical systems put in place today but currently have no voice or input on the subject. High schoolers are able to articulate their perspectives on technology and related subjects very well even if their personal experience is limited, but programs like the City of Boston’s Beta Blocks events have largely failed to reach them to hear their stance.</p>
+    <p>Ponder acts as a means of collecting these unheard perspectives. This data set could prove useful for a variety of stakeholders, including educators and others hoping to engage high school students and other young individuals and educate on the subjects of ethics in technology and data privacy.</p>
   </div>
 );
 
